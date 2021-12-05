115f839e75bdf2f4ea64076b99fda55aff730060
Talha Arıç <talharic@gmail.com>
2021-12-05 21:58:37 +0300

    add google map to screen. Also detect location and shows on map.

--- screens/MapScreen.tsx
+++ screens/MapScreen.tsx
@@ -4,27 +4,58 @@ import { StyleSheet } from 'react-native';
 import { Text, View } from '../components/Themed';
 import { RootTabScreenProps } from '../types';
 
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
+import * as Location from 'expo-location';
+
 export default function MapScreen({ navigation }: RootTabScreenProps<'Map'>) {
+  const [location, setLocation] = React.useState({coords:{latitude: 0, longitude: 0}});
+  React.useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        return;
+      }
+      setLocation(await Location.getCurrentPositionAsync({}));
+      getPharmacy(location.coords.latitude, location.coords.longitude, "Pharmacy");
+    })();
+  }, []);
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Harita buraya gelecek</Text>
+      <MapView
+        provider={PROVIDER_GOOGLE}
+        style={styles.map}
+        showsUserLocation={true}
+        region={{
+          latitude: location.coords.latitude,
+          longitude: location.coords.longitude,
+          latitudeDelta: 0.01,
+          longitudeDelta: 0.001,
+        }}
+      >
+      </MapView>
     </View>
   );
 }
 
+function getPharmacy(latitude: any, longitude: any, keyword: string){
+  let parameters: any = {
+    location: `${latitude},${longitude}`,
+    radius: 1500,
+    keyword: keyword,
+    key: "AIzaSyDiq02ouFmrWs-YCB0QTi4W0pt_BqS1Agw"
+  }
+  let url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?" + Object.keys(parameters).map(key => `${key}=${parameters[key]}`).join("&");
+  console.log(url);
+  fetch(url).then(res => res.json()).then(data => console.log(data)).catch(error => console.log(error));
+}
+
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    ...StyleSheet.absoluteFillObject,
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-end',
   },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
+  map: {
+    ...StyleSheet.absoluteFillObject,
   },
 });
