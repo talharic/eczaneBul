78cc6dfb2450ac3b6120656b93848e4980467684
Talha Arıç <talharic@gmail.com>
2021-12-28 23:55:46 +0300

    add directions to map

--- components/MapScreen.tsx
+++ components/MapScreen.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import {StyleSheet} from 'react-native';
-import {View} from './Themed';
+import {Text, View} from './Themed';
 import MapView, {Marker, PROVIDER_GOOGLE} from 'react-native-maps';
 import * as Location from 'expo-location';
+import FAB from 'react-native-fab';
+import { FontAwesome } from '@expo/vector-icons';
+import MapViewDirections from 'react-native-maps-directions';
 
 export default function MapScreen(props: { term: string }) {
   const [location, setLocation] = React.useState({coords:{latitude: 0, longitude: 0}});
   const [markers, setMarkers] = React.useState([]);
   const [pharmacies, setPharmacies] = React.useState([]);
+  const [destination, setDestination] = React.useState({latitude: 0, longitude: 0});
   const {term} = props;
 
   React.useEffect(() => {
@@ -48,7 +52,7 @@ export default function MapScreen(props: { term: string }) {
     }).then(res => res.json()).then(data => setPharmacies(data["data"]));
   }
 
-  function getPlaces(latitude: any, longitude: any, keyword: string){
+  async function getPlaces(latitude: any, longitude: any, keyword: string){
     let parameters: any = {
       location: `${latitude},${longitude}`,
       radius: 8000,
@@ -56,7 +60,40 @@ export default function MapScreen(props: { term: string }) {
       key: "AIzaSyBvJdlk21u2NUmvJVHdVNxqOMeZOlOTMpc"
     }
     let url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?" + Object.keys(parameters).map(key => `${key}=${parameters[key]}`).join("&");
-    fetch(url).then(res => res.json()).then(data => setMarkers(data.results)).catch(error => console.log(error));
+    await fetch(url).then(res => res.json()).then(data => setMarkers(data.results)).catch(error => console.log(error));
+  }
+
+  async function showDirectionToClosest(){
+    if(destination.latitude != 0 && destination.longitude != 0){
+      setDestination({latitude: 0, longitude: 0});
+      return;
+    }
+    let parameters: any = {
+      origins: `${location.coords.latitude},${location.coords.longitude}`,
+      destinations: (
+          pharmacies.length ?
+            pharmacies.map(item => `${item['latitude']},${item['longitude']}`) :
+            markers.map(item => `${item["geometry"]["location"]["lat"]},${item["geometry"]["location"]["lng"]}`)
+        ).join('|'),
+      key: "AIzaSyBvJdlk21u2NUmvJVHdVNxqOMeZOlOTMpc"
+    }
+    let result : any = [];
+    let url = "https://maps.googleapis.com/maps/api/distancematrix/json?" + Object.keys(parameters).map(key => `${key}=${parameters[key]}`).join("&");
+    await fetch(url).then(res => res.json()).then(data => result = data["rows"][0]["elements"]).catch(error => console.log(error));
+    let index = 0, distance = result[0]["distance"]["value"];
+    for(let i = 1; i < result.length; i++) {
+      if(result[i]["distance"]["value"] < distance){
+        distance = result[i]["distance"]["value"];
+        index = i
+      }
+    }
+    if(index != -1) {
+      parameters["destinations"] = parameters["destinations"].split("|").map((item : any) => item.split(","));
+      setDestination({
+        latitude: parseFloat(parameters["destinations"][index][0]),
+        longitude: parseFloat(parameters["destinations"][index][1])
+      });
+    }
   }
 
   return (
@@ -80,7 +117,20 @@ export default function MapScreen(props: { term: string }) {
               return <Marker key={marker["place_id"]} title={marker["name"]} coordinate={{latitude: marker["geometry"]["location"]["lat"], longitude: marker["geometry"]["location"]["lng"]}} />
             })
         }
+        
+        {destination.latitude !=0 && destination.longitude != 0 && (<MapViewDirections
+            origin={location.coords}
+            destination={destination}
+            apikey={"AIzaSyBvJdlk21u2NUmvJVHdVNxqOMeZOlOTMpc"}
+            strokeWidth={3}
+            strokeColor="blue"
+            optimizeWaypoints={true}
+            onStart={(params) => {
+              console.log(`Started routing between "${params.origin}" and "${params.destination}"`);
+            }}
+          />)}
       </MapView>
+      <FAB buttonColor="black" iconTextColor="#FFFFFF" onClickAction={showDirectionToClosest} visible={true} iconTextComponent={<FontAwesome size={30} style={{ marginBottom: -3 }} name={destination.latitude != 0 && destination.longitude != 0 ? "close" : "road"} />} />
     </View>
   );
 }
